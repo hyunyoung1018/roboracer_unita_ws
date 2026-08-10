3114bc7f9aedf36bf39ab3dd871ab2d9ab6a4b13
UNITA <imsunghy@gmail.com>
2026-08-10 16:18:12 +0900

    update spliner parameter

--- src/planner/spline/spline/spline_node.py
+++ src/planner/spline/spline/spline_node.py
@@ -29,7 +29,7 @@ class SplineNode(Node):
 
         defaults = {
             'lookahead': 5.0,
-            'evasion_distance': 0.2,
+            'evasion_distance': 0.4,
             'trajectory_threshold': 0.6,
             'boundary_margin': 0.20,
             'spline_resolution': 0.10,
